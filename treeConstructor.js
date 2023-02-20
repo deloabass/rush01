51dee6cacda20614dc026dcd9ef2e0bef22e4bca
deloabass <124785233+deloabass@users.noreply.github.com>
2023-02-20 10:41:26 +0100

    Add files via upload

---
+++ treeConstructor.js
@@ -0,0 +1,10 @@
+function TreeConstructor(strArr) {
+    var strArr = prompt (["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"]);
+    if(true) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+  TreeConstructor();
+  
