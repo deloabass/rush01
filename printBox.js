8cd380b78e5b59f0715be47946a875e026c644d7
deloabass <124785233+deloabass@users.noreply.github.com>
2023-02-20 11:19:41 +0100

    Add files via upload

---
+++ printBox.js
@@ -0,0 +1,18 @@
+var box = {
+    width1: "*****",
+    height1:"*   *" ,
+    height2:"*   *" ,
+    width2: "*****",
+
+    printBox: function (height, width) {
+        return this.width1 +
+            '\n' + this.height1 +
+            '\n' + this.height2 +
+            '\n' + this.width2;
+
+    }
+
+}
+
+console.log(box.printBox());
+module.exports = printBox;
